3e53e0e8eddc28ea522c22506e8a7408c6ba3cda
11yyaa <113608806+11yyaa@users.noreply.github.com>
2024-02-15 21:46:50 +0300

    Update vector_operations.cpp

--- src/vector_operations.cpp
+++ src/vector_operations.cpp
@@ -1,13 +1,5 @@
 #include "vector_operations.h"
 
-std::vector<double> &operator+=(std::vector<double> &vec1,
-                                const std::vector<double> &vec2) {
-    assert(vec1.size() == vec2.size() && "vectors must be the same size");
-    for (int i = 0; i < vec1.size(); ++i) {
-        vec1[i] += vec2[i];
-    }
-    return vec1;
-}
 
 std::vector<double> operator+(const std::vector<double> &vec1,
                               const std::vector<double> &vec2) {
@@ -16,11 +8,6 @@ std::vector<double> operator+(const std::vector<double> &vec1,
     return vec_res;
 }
 
-std::vector<double> &operator-=(std::vector<double> &vec1,
-                                const std::vector<double> &vec2) {
-    vec1 += vec2 * (-1);
-    return vec1;
-}
 
 std::vector<double> operator-(const std::vector<double> &vec1,
                               const std::vector<double> &vec2) {
@@ -29,12 +16,6 @@ std::vector<double> operator-(const std::vector<double> &vec1,
     return vec_res;
 }
 
-std::vector<double> &operator*=(std::vector<double> &vec, const double num) {
-    for (int i = 0; i < vec.size(); ++i) {
-        vec[i] *= num;
-    }
-    return vec;
-}
 
 std::vector<double> operator*(const std::vector<double> &vec,
                               const double num) {
