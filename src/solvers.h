5db55f1318dc85616e65604424bd523f5529290a
11yyaa <113608806+11yyaa@users.noreply.github.com>
2024-02-22 21:36:27 +0300

    божечкихелп

--- src/solvers.h
+++
@@ -1,9 +0,0 @@
-#include "matrixes.h"
-
-#ifndef SOLVERS_H
-#define SOLVERS_H
-
-std::vector<double> solve(const TridiagonalMatrix &matrix,
-                          const std::vector<double> &column);
-
-#endif
