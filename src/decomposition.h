637119ed6e3c188a066787f0de2a573393f4870a
11yyaa <113608806+11yyaa@users.noreply.github.com>
2024-02-29 20:39:57 +0300

    Add files via upload

---
+++ src/decomposition.h
@@ -0,0 +1,10 @@
+#ifndef DECOMPOSITION_H
+#define DECOMPOSITION_H
+
+#include "matrixes.h"
+#include "algorithms.h"
+#include "vector_operations.h"
+
+DenseMatrix HouseholderDecompostion(const DenseMatrix &matrix);
+
+#endif
